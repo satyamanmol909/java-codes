75443856c6e2321f1bf5d3bc12788e3cc3085615
Satyam Anmol <anmolsatyam6@gmail.com>
2025-02-21 20:11:14 +0530

    Add files via upload

---
+++ function_even_odd.java
@@ -0,0 +1,19 @@
+public class function_even_odd 
+{
+     static void even_odd(int n)
+    {
+        if(n%2==0){
+            System.out.println(n + " is even number");
+        }
+        else{
+            System.out.println(n+" is odd number");
+        }
+    }
+    public static void main(String[] args)
+    {
+        even_odd(7);
+    }
+    
+
+    
+}
