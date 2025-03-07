a2fc2146cecd2092208f7f43a55a145e1a4b2ce6
Satyam Anmol <anmolsatyam6@gmail.com>
2025-03-07 19:49:54 +0530

    Add files via upload

--- PrimeNumberInRange.java
+++ PrimeNumberInRange.java
@@ -2,18 +2,6 @@ import java.util.Scanner;
 
 public class PrimeNumberInRange {
 
-    public static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the starting number of the range: ");
@@ -21,11 +9,22 @@ public class PrimeNumberInRange {
         System.out.print("Enter the ending number of the range: ");
         int end = scanner.nextInt();
         System.out.println("Prime numbers between " + start + " and " + end + " are:");
+
         for (int i = start; i <= end; i++) {
-            if (isPrime(i)) {
-                System.out.println(i);
+            if (i > 1) {
+                boolean isPrime = true;
+                for (int j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    System.out.println(i);
+                }
             }
         }
         scanner.close();
     }
 }
+
