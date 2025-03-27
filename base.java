2a59b78d31a93cc122f7a987c4ed5a1a6bea788d
Satyam Anmol <anmolsatyam6@gmail.com>
2025-03-27 20:11:37 +0530

    Add files via upload

--- base.java
+++ base.java
@@ -1,19 +1,29 @@
-public class base {
-    int roll = 1;
-    int std = 3;
-    String name = "satyam";
+class base // base class
+{
+    // data of student
+    String name = "satyam anmol";
+    static int roll = 5;
+
+    static void add() {
+        System.out.println("hello");
+    }
+
+    public void information() {
+        System.out.println("hello. i am second student");
+    }
 
 }
 
-class derived extends base {
-    public void out() {
-        System.out.println("NAME=" + name);
-        System.out.println("ROLL=" + roll);
-        System.out.println("class=" + std);
-    }
+class ram extends base //
+{
+    public static void main(String[] args) {
 
-    public static void main(String args[]) {
-        derived d = new derived();
-        d.out();
+        ram r = new ram();
+        r.information();
+        add();
+        r.name = "raj";
+        System.out.println(r.name);
+        System.out.println(roll);
+
     }
 }
